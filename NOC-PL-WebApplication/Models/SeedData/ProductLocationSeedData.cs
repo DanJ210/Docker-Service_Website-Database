7213c80c3ffd8540969053b42ec692272a8be090
Daniel Jackson <djackson@ecnetwork.com>
2017-03-22 16:11:05 -0400

    Added seeder and product seed data.

--- NOC-PL-WebApplication/Models/SeedData/ProductLocationSeedData.cs
+++ NOC-PL-WebApplication/Models/SeedData/ProductLocationSeedData.cs
@@ -15,14 +15,33 @@ namespace NOCPLWebApplication.Models.SeedData {
         //Seeding task that seeds only if data is not already found
         public async Task EnsureSeedData() {
             if (!_context.Products.Any()) {
-                var product = new Product() {
-                    ProductName = "NEXT.coderedweb.com"
+                var productList = new List<Product>(){
+                    new Product { ProductName = "NEXT.Coderedweb.com", ProductGroup = "NEXT" },
+                    new Product { ProductName = "NEXT API", ProductGroup = "NEXT" },
+                    new Product { ProductName = "NEXT CNE", ProductGroup = "NEXT" },
+                    new Product { ProductName = "SmartNotice", ProductGroup = "SMART" },
+                    new Product { ProductName = "medical.smartnotice.net", ProductGroup = "SMART" },
+                    new Product { ProductName = "assurity.smartnotice.net", ProductGroup = "SMART" },
+                    new Product { ProductName = "api.coderedweb.com", ProductGroup = "CODERED" },
+                    new Product { ProductName = "public.coderedweb.com", ProductGroup = "CODERED" },
+                    new Product { ProductName = "geo.ecngateway.com", ProductGroup = "ECNGATEWAY" },
+                    new Product { ProductName = "Dashboard", ProductGroup = "DASHBOARD" },
+                    new Product { ProductName = "CodeEd", ProductGroup = "ED" },
+                    new Product { ProductName = "MyDailyCall", ProductGroup = "DAILY" },
+                    new Product { ProductName = "Portal", ProductGroup = "Portal" },
+                    new Product { ProductName = "demo.coderedweb.com", ProductGroup = "DEMO"}
                 };
+
+                //var product = new List<Product>( { ProductName = "NEXT.coderedweb.com", ProductGroup = "NEXT" });
+                _context.AddRange(productList);
+                await _context.SaveChangesAsync();
             }
             
             if (!_context.Servers.Any()) {
 
             }
+
+            //await _context.SaveChangesAsync();
         }
     }
 }
