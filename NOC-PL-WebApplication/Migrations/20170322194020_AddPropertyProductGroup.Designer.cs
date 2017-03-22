ef6e41b765c6d496ddb2dda117f1e2a4221a5f02
Daniel Jackson <djackson@ecnetwork.com>
2017-03-22 15:42:02 -0400

    Creating database and product/server models

---
+++ NOC-PL-WebApplication/Migrations/20170322194020_AddPropertyProductGroup.Designer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NOCPLWebApplication.Models;
+
+namespace NOCPLWebApplication.Migrations
+{
+    [DbContext(typeof(ProductLocationContext))]
+    [Migration("20170322194020_AddPropertyProductGroup")]
+    partial class AddPropertyProductGroup
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("NOCPLWebApplication.Models.Product", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ProductGroup");
+
+                    b.Property<string>("ProductName");
+
+                    b.Property<int?>("ServerId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("ServerId");
+
+                    b.ToTable("Products");
+                });
+
+            modelBuilder.Entity("NOCPLWebApplication.Models.Server", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ServerName");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Servers");
+                });
+
+            modelBuilder.Entity("NOCPLWebApplication.Models.Product", b =>
+                {
+                    b.HasOne("NOCPLWebApplication.Models.Server")
+                        .WithMany("ProductsContained")
+                        .HasForeignKey("ServerId");
+                });
+        }
+    }
+}
---
+++ NOC-PL-WebApplication/Models/SeedData/ProductLocationSeedData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOCPLWebApplication.Models.SeedData {
+    public class ProductLocationSeedData {
+        private ProductLocationContext _context;
+
+        public ProductLocationSeedData(ProductLocationContext context) {
+            _context = context;
+        }
+        
+        //Seeding task that seeds only if data is not already found
+        public async Task EnsureSeedData() {
+            if (!_context.Products.Any()) {
+                var product = new Product() {
+                    ProductName = "NEXT.coderedweb.com"
+                };
+            }
+            
+            if (!_context.Servers.Any()) {
+
+            }
+        }
+    }
+}
