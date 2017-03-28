bf609475d82b01bfd99afad1044f1e942f940cc9
Daniel Jackson <djackson@ecnetwork.com>
2017-03-28 12:35:38 -0400

    Adding a third model

---
+++ NOC-PL-WebApplication/Models/TableData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NOCPLWebApplication.Models {
+    public class TableData {
+        public int Id { get; set; }
+        public int TableNumber { get; set; }
+        public ICollection<Product> TableProducts { get; set; }
+        public ICollection<Server> TableServers { get; set; }
+    }
+}
