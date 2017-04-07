a3eb4cdf7e06dad0c1f26822cbdc69465ee6331a
Daniel Jackson <djackson@ecnetwork.com>
2017-04-07 16:48:03 -0400

    Adding jsDoc summary comments

--- NOC-PL-WebApplication/wwwroot/js/serverModal.Controller.js
+++ NOC-PL-WebApplication/wwwroot/js/serverModal.Controller.js
@@ -4,16 +4,22 @@
     var serverColumn;
     var serverGroup;
 
-    $(document).ready(function () {
+    /**
+         * @desc Sets up logic for modal window. Creates jquery method to listen
+         *       for a change on the server select list in the modal window.
+         * @param  {string} productName
+         * @param  {int} productId
+         * @param  {string} serverColumn
+         * @param  {string} serverGroup
+         */
 
+    $(document).ready(function () { 
         
         $('[serverColumn]').click(function () {
             productName = $(this).attr('productName');
             serverColumn = $(this).attr('serverColumn');
             serverGroup = $(this).attr('serverGroup'); // Used in future iteration for color grouping of servers
             productId = $(this).attr('productId');
-            //alert($(this).attr('productid'));
-            alert($(this).attr('productId'));
 
             $('#serverModal').modal();
 
@@ -23,22 +29,25 @@
             // Keeping this seperate makes it work faster.
             $('#serverModal').on('hide.bs.modal', function () {
                 location.reload();
-                //alert($('#container-fluid').get('localhost'));
             });
         });
     });
 
     $.fn.serverListChangeFunction = function serverListChange() {
+        /**
+         * @desc jquery function to save selected server to database
+         * @param {int} serverId
+         */
         var serverId = $('#ServerSelectList').val();
         $.post("products/SaveSelectedServer",
             {
                 productId: productId,
                 serverColumn: serverColumn,
                 serverId: serverId
-            },
-            function (data, status) {
-                alert(data + "status" + status);
             });
+            // function (data, status) {
+            //     alert(data + "status" + status);
+            // });
     }
 }(jQuery));
 
