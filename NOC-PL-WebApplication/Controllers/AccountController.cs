08bf8a1cf6bad63bf8a2baad17f6ed0bd36ef14d
Daniel Jackson <djackson@ecnetwork.com>
2017-04-12 14:11:35 -0400

    Adding LoginLogout component

--- NOC-PL-WebApplication/Controllers/AccountController.cs
+++ NOC-PL-WebApplication/Controllers/AccountController.cs
@@ -5,25 +5,54 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using NOCPLWebApplication.Models;
+using NOCPLWebApplication.Models.ViewModels;
+using Microsoft.Extensions.Logging;
 
 namespace NOC_PL_WebApplication.Controllers {
     public class AccountController : Controller {
         private UserManager<NocUser> _userManager;
         private SignInManager<NocUser> _signInManager;
         private ProductLocationContext _context;
+        private ILogger<AccountController> _logger;
 
-        public AccountController(ProductLocationContext context, UserManager<NocUser> userManager, SignInManager<NocUser> signInManager) {
+        public AccountController(ProductLocationContext context, UserManager<NocUser> userManager, SignInManager<NocUser> signInManager, ILogger<AccountController> logger) {
             _context = context;
             _userManager = userManager;
             _signInManager = signInManager;
-        }
-        public IActionResult Index() {
-            return View();
+            _logger = logger;
         }
 
         [HttpGet]
         public async Task<IActionResult> Login() {
             return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model) {
+            try {
+                if (ModelState.IsValid) {
+                    var loginResult = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false);
+
+                    if (loginResult.Succeeded) {
+                        if (Url.IsLocalUrl(model.ReturnUrl)) {
+                            return Redirect(model.ReturnUrl);
+                        } else {
+                            return RedirectToAction("TableDataVms", "Index");
+                        }
+                    }
+                }
+                
+            } catch(Exception ex) {
+                _logger.LogError($"Could not sign in user with message: {ex.Message}");
+            }
+            ModelState.AddModelError("", "Could not login");
+            return View(model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout() {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("TableDataVMs", "Index");
         }
     }
 }
