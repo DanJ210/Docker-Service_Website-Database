53c10fbc122dd40499e8f2eb3313fc29176e21a7
Daniel Jackson <daniel.jackson@onsolve.com>
2017-09-22 16:59:31 -0400

    Code refactoring and code review standards to web files.

--- NocWebUtilityApp/published/wwwroot/js/serverModal.Controller.js
+++ NocWebUtilityApp/published/wwwroot/js/serverModal.Controller.js
@@ -1,66 +1,73 @@
-﻿; (function ($) {
-    var productId;
-    var serverColumn;
-    var serverGroup;
-    
-    /**
-    * @desc Logic for Modal Window. Gets parameters for server call. 
-    * @param  {int} productId
-    * @param  {string} serverColumn
-    * @param  {string} serverGroup
-    */
-    $(document).ready(function () { 
-        
-        $('[serverColumn]').click(function () {
-            serverColumn = $(this).attr('serverColumn');
-            serverGroup = $(this).attr('serverGroup'); // Used in future iteration for color grouping of servers
-            productId = $(this).attr('productId');
+﻿//* OnSolve
+//* Author: Daniel Jackson
+//* NocWebUtility Application
+//* Created: 03 / 22 / 2017
+//* Last Edit: 09 / 22 / 2017
+//* Description: Logic for the modal window.
 
-            $('#serverModal').modal();
+; (function ($) {
+	var productId;
+	var serverColumn;
+	var serverGroup;
 
-            $('#saveButton').click(function () {
-                $('#serverModal').modal('hide');
-            });
-            // Keeping this seperate makes it work faster.
-            $('#serverModal').on('hide.bs.modal', function () {
-                
-            });
-        });
-    });
+	/**
+	* @desc Gets parameters for server call. 
+	* @param  {int} productId
+	* @param  {string} serverColumn
+	* @param  {string} serverGroup
+	*/
+	$(document).ready(function () {
 
-    $.fn.serverListChangeFunction = function serverListChange() {
-        /**
-         * @desc POSt
-         * @param {int} selectedServerId
-         * @param {int[]} selectedProductIds
-         * @param {Controller} TableDataVMs
-         * @param {Action} SaveSelectedServer
-         */
-        var selectedServerId = $('#ServerSelectList').val();
-        var selectedProductIds = $('#ProductSelectList').val();
-        //alert(selectedProductIds);
-        var ajaxRequest = $.ajax({
-            type: 'POST',
-            url: 'SaveSelectedServer',
-            data: {
-                'productId': productId,
-                'serverColumn': serverColumn,
-                'serverId' : selectedServerId},
-            success: function () {
-                $('#serverModal').modal('hide');
-                location.reload();
-            },
-            error: function (status) {
-                if (status.status === 401) {
-                    alert("Data can only be changed by admin");
-                }
-                else {
-                    alert(status.statusText + ". Please contact admin");
-                }
-                $('#serverModal').modal('hide');
-            }
-        });
-    };
+		$('[serverColumn]').click(function () {
+			serverColumn = $(this).attr('serverColumn');
+			serverGroup = $(this).attr('serverGroup'); // Used in future iteration for color grouping of servers
+			productId = $(this).attr('productId');
+
+			$('#serverModal').modal();
+
+			$('#saveButton').click(function () {
+				$('#serverModal').modal('hide');
+			});
+			// Keeping this seperate makes it work faster.
+			$('#serverModal').on('hide.bs.modal', function () {
+
+			});
+		});
+	});
+
+	$.fn.serverListChangeFunction = function serverListChange() {
+		/**
+		 * @desc POST new server change when user selects.
+		 * @param {int} selectedServerId
+		 * @param {int[]} selectedProductIds
+		 * @param {Controller} TableDataVMs
+		 * @param {Action} SaveSelectedServer
+		 */
+		var selectedServerId = $('#ServerSelectList').val();
+		var selectedProductIds = $('#ProductSelectList').val();
+		var ajaxRequest = $.ajax({
+			type: 'POST',
+			url: 'SaveSelectedServer',
+			data: {
+				'productId': productId,
+				'serverColumn': serverColumn,
+				'serverId': selectedServerId
+			},
+			success: function () {
+				$('#serverModal').modal('hide');
+				location.reload();
+			},
+			error: function (status) {
+				if (status.status === 401) {
+					alert("Data can only be changed by admin");
+				}
+				else {
+					alert(status.statusText + ". Please contact admin");
+				}
+				$('#serverModal').modal('hide');
+			}
+		});
+	};
 }(jQuery));
 
 
